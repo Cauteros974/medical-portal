9566ffeac64a406760edc430153dc906fa4ef213
Cauteros974 <pusik6666@ukr.net>
2025-09-19 17:57:32 +0300

    Create BookingForm.jsx

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,24 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+import { useDropzone } from "react-dropzone";
+
+const BookingForm = ({ doctorName }) => {
+    const { acceptedFiles, getRootProps, getInputProps} = useDropzone();
+    const files = acceptedFiles.map(file => <li key={file.path}>{file.path}</li>);
+
+    return(
+        <>
+        <h3>Make an appointment with {doctorName}</h3>
+        <Formik 
+            initialValues={{ name: '', photo: '', date: ''}}
+            validationSchema={Yup.object({
+                name: Yup.string().required("Required field"),
+                phone: Yup.string().required("Required field"),
+                date: Yup.string().required("Choose date"),
+            })}
+        />
+        </>
+    )
+};
+
+export default BookingForm;
