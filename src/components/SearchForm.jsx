ad51a9e50b07f597445796b51c50282e1cff31a2
Cauteros974 <pusik6666@ukr.net>
2025-09-19 17:46:14 +0300

    Update SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,11 +1,14 @@
-const SearchForm = ({ onSearch}) => {
-    return(
-        <form className="search-form" onSubmit={(e) => e.preventDefault}>
-            <input 
-                type="name"
-            />
-        </form>
-    )
-};
-
-export default SearchForm;
+const SearchForm = ({ onSearch }) => {
+    return (
+      <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Search by name or specialty..."
+          className="search-input"
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </form>
+    );
+  };
+  
+  export default SearchForm;
