2e4b7eaddfa8b753c044cc0110937e8264d96dea
Cauteros974 <pusik6666@ukr.net>
2025-09-19 18:49:28 +0300

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+const AboutUs = () => (
+    <div className="page-container">
+        <div className="form-wrapper">
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem'}}>About our project</h1>
+            <p>
+                MedPortal is a modern platform designed to simplify the process of finding doctors and making appointments. 
+                Our mission is to make quality medical care more accessible to everyone. 
+                We believe that technology can help patients and doctors find each other faster and more efficiently.
+            </p>
+        </div>
+    </div>
+);
+
+export default AboutUs;
