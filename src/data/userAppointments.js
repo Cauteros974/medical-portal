816075a00baad8e0a34ea034ee25960666f59e8d
Cauteros974 <pusik6666@ukr.net>
2025-09-19 17:23:06 +0300

    commit

---
+++ src/data/userAppointments.js
@@ -0,0 +1,4 @@
+export const userAppointments = [
+    { id: 1, doctorId: 2, doctorName: "Dr. Lorenzo Ferraro", specialty: "Neurologist", date: "2025-10-20T10:00:00" },
+    { id: 2, doctorId: 3, doctorName: "Dr. Giulia Moretti", specialty: "Pediatrician", date: "2025-11-05T15:30:00" },
+  ];
