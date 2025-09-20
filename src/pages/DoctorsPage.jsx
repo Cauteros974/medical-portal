f455c666188049d044688ffab7032ea0305a94e9
Cauteros974 <pusik6666@ukr.net>
2025-09-20 21:56:44 +0300

    Update DoctorsPage.jsx

--- src/pages/DoctorsPage.jsx
+++ src/pages/DoctorsPage.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react";
-import SearchForm from "../components/SearchForm";
-import DoctorCards from "../components/DoctorCards";
+import { useState, useEffect } from 'react';
+import SearchForm from '../components/SearchForm';
+import DoctorCard from '../components/DoctorCard';
 import { doctors as mockDoctors } from '../data/doctors';
 
-const DoctorPage = () => {
-    const [ doctors, setDoctors ] = useState([]);
-    const [ filteredDoctors, setFilteredDoctors ] = useState([]);
+const DoctorsPage = () => {
+  const [doctors, setDoctors] = useState([]);
+  const [filteredDoctors, setFilteredDoctors] = useState([]);
 
-    useEffect(() => {
-        setDoctors(mockDoctors);
-        setFilteredDoctors(filteredDoctors);
-    }, []);
-
-    const handleSearch = (query) => {
-        const lowercasedQuery = query.toLowerCase;
-        const result = doctors.diltered(doctors => 
-            doctor.name.toLowerCase().includes(lowercasedQuery) ||
-            doctor.specialty.toLowerCase().includes(lowercasedQuery)
-        );
-        setFilteredDoctors(result);
-    };
-
-    return(
-        <div className="page-container">
-            <SearchForm onSearch={handleSearch} />
-            <div className="doctors-grid">
-                {filteredDoctors.map(doctor => (
-                    <DoctorCards key={doctor.id} doctor={doctor}/>
-                ))}
-            </div>
-        </div>
+  useEffect(() => {
+    setDoctors(mockDoctors);
+    setFilteredDoctors(mockDoctors);
+  }, []);
+  
+  const handleSearch = (query) => {
+    const lowercasedQuery = query.toLowerCase();
+    const result = doctors.filter(doctor => 
+      doctor.name.toLowerCase().includes(lowercasedQuery) ||
+      doctor.specialty.toLowerCase().includes(lowercasedQuery)
     );
+    setFilteredDoctors(result);
+  };
+
+  return (
+    <div className="page-container">
+      <SearchForm onSearch={handleSearch} />
+      <div className="doctors-grid">
+        {filteredDoctors.map(doctor => (
+          <DoctorCard key={doctor.id} doctor={doctor} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default DoctorPage;
+export default DoctorsPage;
