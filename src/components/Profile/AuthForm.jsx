477f0eb59d219e4a8daf54810a0cbb8c5f360ab9
Cauteros974 <pusik6666@ukr.net>
2025-09-20 18:10:31 +0300

    Create AuthForm.jsx

---
+++ src/components/Profile/AuthForm.jsx
@@ -0,0 +1,14 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const AuthForm = ({ onLogin }) => {
+    <div className="form-wrapper">
+        <h2>Login or Registration</h2>
+        <Formik>
+            initialValues = {{ email: '', password: ''}}
+            validationSchema={Yup.object({
+        email: Yup.string().email('Invalid email').required("Required field"),
+        password: Yup.string().min(6, 'Password is too short').required("Required field"),
+      })}
+        </Formik>
+    </div>
+}
