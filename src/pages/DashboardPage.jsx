9a3f3e1e941919fb15516fea8132a5b3819a673a
Cauteros974 <pusik6666@ukr.net>
2025-09-20 22:11:44 +0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,20 +1,20 @@
 import Profile from "../components/Profile/Profile";
 import { userAppointments } from "../data/userAppointments";
 
-const DashboardPage = () => {
+const DashboardPage = ({ user, onLogout }) => (
     <div className="page-container dashboard">
-        <Profile user={user} onLogOut={onLogOut} />
-
-        <div style={{ marginTop: '2rem' }}>
-            <h3>Your future entries</h3>
-            {userAppointments.map(appt => (
-                <div key={appt.id} className="appointment-card">
-                    <p><strong>Doctor:</strong> {appt.doctorName} ({appt.specialty})</p>
-                    <p><strong>Data:</strong> {new Date(appt.date).toLocaleString('uk-UA')}</p>
-                </div>
-      ))}
+      <Profile user={user} onLogout={onLogout} />
+      
+      <div style={{ marginTop: '2rem' }}>
+        <h3>Your future entries</h3>
+        {userAppointments.map(appt => (
+          <div key={appt.id} className="appointment-card">
+            <p><strong>Doctor:</strong> {appt.doctorName} ({appt.specialty})</p>
+            <p><strong>Data:</strong> {new Date(appt.date).toLocaleString('uk-UA')}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-};
+  );
 
 export default DashboardPage;
