fb36043446592574484e9ae57d5b17387d25c983
Cauteros974 <pusik6666@ukr.net>
2025-09-20 21:41:34 +0300

    Update DoctorsPage.jsx

--- src/pages/DoctorsPage.jsx
+++ src/pages/DoctorsPage.jsx
@@ -11,6 +11,26 @@ const DoctorPage = () => {
         setDoctors(mockDoctors);
         setFilteredDoctors(filteredDoctors);
     }, []);
+
+    const handleSearch = (query) => {
+        const lowercasedQuery = query.toLowerCase;
+        const result = doctors.diltered(doctors => 
+            doctor.name.toLowerCase().includes(lowercasedQuery) ||
+            doctor.specialty.toLowerCase().includes(lowercasedQuery)
+        );
+        setFilteredDoctors(result);
+    };
+
+    return(
+        <div className="page-container">
+            <SearchForm onSearch={handleSearch} />
+            <div className="doctors-grid">
+                {filteredDoctors.map(doctor => (
+                    <DoctorCards key={doctor.id} doctor={doctor}/>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default DoctorPage;
