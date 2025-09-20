d0e45479eadb8ec695ac3bef62e099a709b20f7d
Cauteros974 <pusik6666@ukr.net>
2025-09-20 21:49:58 +0300

    commit

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,2 +1,8 @@
 import Profile from "../components/Profile/Profile";
 import { userAppointments } from "../data/userAppointments";
+
+const DashboardPade = ({ user, onLogOut}) => {
+    <div className="page-container dashboard">
+        <Profile user={user} onLogOut={onLogOut} />
+    </div>
+}
