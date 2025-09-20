30669ac15ed5c17de4987cfa45d93ad044c4a600
Cauteros974 <pusik6666@ukr.net>
2025-09-20 22:11:13 +0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,6 +4,16 @@ import { userAppointments } from "../data/userAppointments";
 const DashboardPage = () => {
     <div className="page-container dashboard">
         <Profile user={user} onLogOut={onLogOut} />
+
+        <div style={{ marginTop: '2rem' }}>
+            <h3>Your future entries</h3>
+            {userAppointments.map(appt => (
+                <div key={appt.id} className="appointment-card">
+                    <p><strong>Doctor:</strong> {appt.doctorName} ({appt.specialty})</p>
+                    <p><strong>Data:</strong> {new Date(appt.date).toLocaleString('uk-UA')}</p>
+                </div>
+      ))}
+    </div>
     </div>
 };
 
