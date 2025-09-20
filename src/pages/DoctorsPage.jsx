24e56e4e7239c3533f0c7bb0eef6fa0d066be6a0
Cauteros974 <pusik6666@ukr.net>
2025-09-20 21:36:18 +0300

    Update DoctorsPage.jsx

--- src/pages/DoctorsPage.jsx
+++ src/pages/DoctorsPage.jsx
@@ -1,4 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchForm from "../components/SearchForm";
 import DoctorCards from "../components/DoctorCards";
 import { doctors as mockDoctors } from '../data/doctors';
+
+const DoctorPage = () => {
+    const [ doctors, setDoctors ] = useState([]);
+    const [ filteredDoctors, setFilteredDoctors ] = useState([]);
+
+    useEffect(() => {
+        setDoctors(mockDoctors);
+        setFilteredDoctors(filteredDoctors);
+    }, []);
+};
+
+export default DoctorPage;
