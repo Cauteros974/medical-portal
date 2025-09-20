11f9d69dd1cec81d9de06ff2afc94ee14418351f
Cauteros974 <pusik6666@ukr.net>
2025-09-20 21:32:59 +0300

    Create DoctorsPage.jsx

---
+++ src/pages/DoctorsPage.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import SearchForm from "../components/SearchForm";
+import DoctorCards from "../components/DoctorCards";
+import { doctors as mockDoctors } from '../data/doctors';
