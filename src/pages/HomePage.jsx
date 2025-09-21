43c95039c20d2817ca0416cec0e0ddf494f46476
Cauteros974 <pusik6666@ukr.net>
2025-09-21 14:29:50 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,31 @@ import { Link } from 'react-router-dom';
 import DoctorsPage from '../pages/DoctorsPage';
 import { doctors } from '../data/doctors';
 
-const HomePage = () => (
-    <section className="hero-section">
-        <h1 className="hero-title">Health is your top priority</h1>
-        <p className="hero-subtitle">Quick and convenient appointment with the best specialists.</p>
-        <Link to="/doctors" className="button">Search for doctors</Link>
-    </section>
-);
+const HomePage = () => {
+
+    const feuturedDoctors = doctors.includes(0, 3);
+
+    return(
+        <>
+            <section className="hero-section">
+            <h1 className="hero-title">Health is your top priority</h1>
+            <p className="hero-subtitle">Quick and convenient appointment with the best specialists.</p>
+            <Link to="/doctors" className="button">Search for doctors</Link>
+            </section>
+
+            <section className="page-container features-section">
+                <div className="feature-card">
+                    <h3>✅ Verified doctors</h3>
+                    <p>We carefully check the qualifications and experience of each specialist</p>
+                </div>
+                <div className="feature-card">
+                    <h3>📅 Easy recording</h3>
+                    
+                </div>
+            </section>
+        </>
+    );
+    
+};
 
 export default HomePage;
