3e6745ed1887865402424797fefc70013ec155ee
Cauteros974 <pusik6666@ukr.net>
2025-09-21 20:19:21 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="footer">
-        <p>&copy; {new Date().getFullYear()} MedPortal. All rights reserved</p>
+        <p>&copy; {new Date().getFullYear()} MedPortal. </p>
       </footer>
     );
   };
