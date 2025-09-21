ad558488a620aa0c826c4764e4eeea8e303b5c29
Cauteros974 <pusik6666@ukr.net>
2025-09-21 14:20:16 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import DoctorsPage from '../pages/DoctorsPage';
+import { doctors } from '../data/doctors';
 
 const HomePage = () => (
     <section className="hero-section">
-        <h1 className="hero-title">Found your doctor</h1>
+        <h1 className="hero-title">Health is your top priority</h1>
         <p className="hero-subtitle">Quick and convenient appointment with the best specialists.</p>
         <Link to="/doctors" className="button">Search for doctors</Link>
     </section>
