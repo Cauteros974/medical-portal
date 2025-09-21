7ce1d4e98c0ce50e3f6367664cd17aae9c9d95be
Cauteros974 <pusik6666@ukr.net>
2025-09-21 20:50:39 +0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../components/Profile/Profile";
 import { userAppointments } from "../data/userAppointments";
 
 const DashboardPage = ({ user, onLogout }) => {
+
+  const [userPhoto, setUserPhoto] = useState(null);
+
+  const handlePhotoUpload = (file) => {
+    const photoURL = URL.createObjectURL(file);
+    setUserPhoto(photoURL);
+    console.log("Photo uploaded:", file.name);
+  };
+
+  const updatedUser = { ...user, photo: userPhoto };
+
+  return(
     <div className="page-container dashboard">
-      <Profile user={user} onLogout={onLogout} />
-      
-      <div style={{ marginTop: '2rem' }}>
-        <h3>Your future entries</h3>
-        {userAppointments.map(appt => (
-          <div key={appt.id} className="appointment-card">
-            <p><strong>Doctor:</strong> {appt.doctorName} ({appt.specialty})</p>
-            <p><strong>Data:</strong> {new Date(appt.date).toLocaleString('uk-UA')}</p>
-          </div>
-        ))}
-      </div>
+      <Profile
+        user={updatedUser}
+        onLogout={onLogout}
+        onPhotoUpload={handlePhotoUpload}
+      />
     </div>
+  );
 };
 
 export default DashboardPage;
