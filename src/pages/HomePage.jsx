08fab87fe43ac1db3c2c849617fde6e211b57887
Cauteros974 <pusik6666@ukr.net>
2025-09-24 19:00:12 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -114,7 +114,7 @@ const HomePage = () => {
                       <h4>Pediatrics</h4>
                       <h3>Seasonal allergies in children: what parents need to know</h3>
                       <p>How to recognize allergy symptoms and help your child feel better...</p>
-                      <Link to="/doctors" className="button">View all doctors</Link>
+                      <a href="#">Read more...</a>
                   </div>
                 </div>
           </div>
