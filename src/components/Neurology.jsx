fbe89f4da7412aec7a8ba4047abbb50cece2bc2c
Cauteros974 <pusik6666@ukr.net>
2025-09-24 21:27:48 +0300

    Update Neurology.jsx

--- src/components/Neurology.jsx
+++ src/components/Neurology.jsx
@@ -7,7 +7,28 @@ const Neurology = () => (
                 strengthens the immune system, and helps maintain a healthy weight and balanced mood. Quality sleep enhances cognitive 
                 functions like memory and focus, reduces stress, lowers the risk of chronic conditions such as heart disease and diabetes,
                 and improves overall alertness and well-being. 
-            </p>            
+            </p>
+
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}></h1>
+            <h2>Here are five simple, benefits of getting sufficient sleep:</h2>
+            <p>1.Boosts Your Immune System Quality sleep helps the body produce protective 
+                cytokines and other substances that fight infection and inflammation, making 
+                you less likely to get sick
+            </p>
+
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}></h1>
+            <p>
+                2.Maintains a Healthy Weight Sufficient sleep helps regulate hormones that control 
+                appetite, and people who sleep enough tend to consume fewer calories and have more 
+                motivation to exercise. 
+            </p>
+
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}></h1>
+            <p>
+                3.Improves Mental Health and Mood Adequate sleep is vital for emotional regulation, 
+                helping to reduce stress, irritability, and mood swings, and improving your overall 
+                emotional well-being.
+            </p>
         </div>
     </div>
 );
