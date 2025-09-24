d5e772e0220ad7a56d721faaae4c737d25cb3e2a
Cauteros974 <pusik6666@ukr.net>
2025-09-24 21:23:04 +0300

    commit

--- src/components/Neurology.jsx
+++ src/components/Neurology.jsx
@@ -1,5 +1,15 @@
-const Neurology = () => {
-    
-};
+const Neurology = () => (
+    <div className="page-container">
+        <div className="sleep-health">
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}>Neurology</h1>
+            <p>
+                Getting enough sleep is crucial for your physical and mental health because it supports the body's repair processes, 
+                strengthens the immune system, and helps maintain a healthy weight and balanced mood. Quality sleep enhances cognitive 
+                functions like memory and focus, reduces stress, lowers the risk of chronic conditions such as heart disease and diabetes,
+                and improves overall alertness and well-being. 
+            </p>            
+        </div>
+    </div>
+);
 
 export default Neurology;
