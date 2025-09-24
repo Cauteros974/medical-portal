3c255cd4b62f040931b2553e8b1b05ac15a5be9d
Cauteros974 <pusik6666@ukr.net>
2025-09-24 21:10:37 +0300

    Update Cardiology.jsx

--- src/components/Cardiology.jsx
+++ src/components/Cardiology.jsx
@@ -3,20 +3,31 @@ const Cardiology = () => (
         <div className="health-advice">
             <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}>Cardiology</h1>
             <p>
-                1. Get Active: Aim for at least 150 minutes of moderate physical 
+                1. <strong>Get Active:</strong> Aim for at least 150 minutes of moderate physical 
                 activity per week to help lower blood pressure, improve cholesterol, and control your weight.
             </p>
             
             <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}></h1>
             <p>
-                2.Eat a Heart-Healthy Diet: Focus on whole foods like fruits, vegetables, whole grains, and lean protein sources. 
+                2.<strong>Eat a Heart-Healthy Diet:</strong> Focus on whole foods like fruits, vegetables, whole grains, and lean protein sources. 
                 Limit your intake of high-sodium, high-fat, and processed foods. 
             </p>
 
             <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}></h1>
             <p>
-                3.Don't Smoke: Smoking significantly increases your risk of heart disease, 
+                3.<strong>Don't Smoke:</strong> Smoking significantly increases your risk of heart disease, 
                 so quitting can dramatically lower your blood pressure and heart rate. 
+            </p>
+
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem'}}></h1>
+            <p>
+                4.<strong>Maintain a Healthy Weight:</strong> Staying at a healthy weight reduces your risk of heart disease. 
+                A combination of a healthy diet and regular exercise is key to achieving and maintaining a healthy weight. 
+            </p>
+
+            <h1 style={{ textAlign: 'center', marginBottom: '1rem'}}></h1>
+            <p>
+                5.<strong>Manage </strong> Stress and Get Enough Sleep: Chronic stress can contribute to high blood pressure and other heart risks. Aim for 7-8 hours of quality sleep per night to allow your body to repair and recover, which also helps control weight and blood pressure. 
             </p>
         </div>
         
